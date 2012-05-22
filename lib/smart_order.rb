412edb8589eb196359f782a51571fa825855b012
Ram Dobson <ram.dobson@solsystemscompany.com>
2012-05-22 15:26:19 -0400

    support legacy interface

--- lib/smart_order.rb
+++ lib/smart_order.rb
@@ -1,6 +1,7 @@
 class ActiveRecord::Base
-  def self.smart_order(order)
-    key, desc = order.sub(/( desc)$/i,''), !!$1
+  def self.smart_order(order, desc = nil)
+    key, inline_desc = order.sub(/( desc)$/i,''), !!$1
+    desc = inline_desc if desc.nil?
     last_class = self
     joins = []
     conditions = []
