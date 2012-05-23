a3eecd1ee12ea7ad30e87d9684b5b8568ef32a36
Caleb Thompson <github@chapso.org>
2012-05-23 10:44:29 -0300

    Explain excusemewtfbbq

--- lib/smart_order.rb
+++ lib/smart_order.rb
@@ -1,5 +1,6 @@
 class ActiveRecord::Base
   def self.smart_order(order, desc = nil)
+    # capture the sort column and whether sorting should be reversed (descending)
     key, inline_desc = order.sub(/( desc)$/i,''), !!$1
     desc = inline_desc if desc.nil?
     last_class = self
