d0b8525a5d8a889e4ed0e2c1d4af5279e42424ea
Caleb Thompson <cjaysson@gmail.com>
2012-10-21 12:43:09 -0400

    Add testing boilerplate
    * Add dummy app for integration testing
    * Add cucumber
    * Add factory_girl
    * Add rspec
    * Add database_cleaner

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '783fb4fbd8421efb74844ee71741d55aee3ea04b13281e31c09b7e7ff8cc90b43cca7ccd720add8fa7681d4feb320b71e87cf3d9c3dce01c7fe466146f37c095'
